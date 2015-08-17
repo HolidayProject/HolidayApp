30717f54b27be1589df26987b92656d621c80b19
swasanka <swasanka@gmail.com>
2015-08-17 18:14:27 +0200

    Tarini commit

--- app/src/androidTest/java/cognizant/com/holidayapp/ApplicationTest.java
+++ app/src/androidTest/java/cognizant/com/holidayapp/ApplicationTest.java
@@ -6,6 +6,7 @@ import android.test.ApplicationTestCase;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  * pulltest
+ * tarini test
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
